029f77503e1c797febfce752e5f477f50ef6570a
Sajjad Zafar <73673391+sajjad2881@users.noreply.github.com>
2023-04-02 14:05:42 -0400

    Gamma stage API gateway

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://rq9uhpyvke.execute-api.us-east-1.amazonaws.com/beta';
+    var invokeUrl = 'https://rq9uhpyvke.execute-api.us-east-1.amazonaws.com/gamma';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
